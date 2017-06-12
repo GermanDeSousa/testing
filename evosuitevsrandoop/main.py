d139246a9e5b9d161ae41cd94b595a49356df51b
GermanDeSousa <gdesousa@eryxsoluciones.com.ar>
2017-06-11 21:25:19 -0300

    main para entregar y creacion de archivo pitest

--- evosuitevsrandoop/main.py
+++ evosuitevsrandoop/main.py
@@ -15,11 +15,24 @@ def main():
 
     for class_name in class_list:
         for i in range(30):
+            build_directory(i, class_name)
             evosuite_command = "java -jar {evosuite_location} -Dsearch_budget=60 -projectCP {binary_location} " \
-                               "-class {class_name}  -base_dir=".format(
+                               "-base_dir evosuite-result/{directory} -class {class_name}".format(
                 evosuite_location=evosuite_location, binary_location=binary_location, class_name=class_name,
                 directory=class_name + str(i))
 
             os.system(evosuite_command)
 
-    os.system('mvn clean install org.pitest:pitest-maven:mutationCoverage')
+    for class_name in class_list:
+        for j in range(30):
+            class_name_to_output_file = class_name.split('.')[2]
+            randoop_command = 'java -ea -classpath {randoop_location}:target/ randoop.main.Main gentests --testclass={class_name}' \
+                              ' --timelimit=60 --testsperfile=20000 --junit-output-dir=src/test/java/randoop ' \
+                              '--regression-test-basename="{regression_file_name}" ' \
+                              '--error-test-basename="{error_file_name}"'.format(randoop_location=randoop_location,
+                                                                                 regression_file_name="RegressionTest" + class_name_to_output_file + str(
+                                                                                     j) + 'A',
+                                                                                 error_file_name="ErrorTest" + class_name_to_output_file + str(
+                                                                                     j) + 'A',
+                                                                                 class_name=class_name)
+            os.system(randoop_command)
